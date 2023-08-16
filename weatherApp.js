afd0fa760ed93d6ceaf31c129af2ef566a8b83a2
Jamaul Alexander Aaron <127161564+jam-carter@users.noreply.github.com>
2023-08-16 13:11:53 +0800

    first version updates

---
+++ weatherApp.js
@@ -0,0 +1,28 @@
+const container = document.querySelector('.container');
+const search = document.querySelector('.search-button');
+const weatherInfo = document.querySelector('.content-weather-info');
+
+search.addEventListener('click', (event) => {
+    event.preventDefault();
+    console.log('click');
+    const APIkey = '465e30e7252001f81fd263141c141bf3';
+    const cityName = document.querySelector('.search-bar').value;
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIkey}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            const weatherInfo = document.querySelector('.content-weather-info');
+            weatherInfo.innerHTML = `
+                <p class="weather-city-name">${data.name}</p>
+                <p class="weather-temperature">${data.main.temp}°C</p>
+                <p class="weather-description">${data.weather[0].description}</p>
+                <p class="weather-humidity">${data.main.humidity}%</p>
+                <p class="weather-wind">${data.wind.speed} m/s</p>
+            `;
+        })
+        .catch(error => {
+            console.log(error);
+        })
+});
+
